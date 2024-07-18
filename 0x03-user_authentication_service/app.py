8a06bf8b288e9fdb64cdc76e9979dc39dcf25fcc
Collinsgra <kipkemoicollins941@gmaill.com>
2024-07-18 09:53:35 -0700

    app.py

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+"""API Routes for auths"""
+from auth import Auth
+from flask import (Flask,
+                   jsonify,
+                   request,
+                   abort,
+                   redirect)
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/', methods=['GET'])
+def hello_world() -> str:
+    """ auth """
+    msg = {"message": "Bienvenue"}
+    return jsonify(msg)
+
+
+@app.route('/users', methods=['POST'])
+def register_user() -> str:
+    """Registers new users"""
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)
+
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    msg = {"email": email, "message": "user created"}
+    return jsonify(msg)
+
+
+@app.route('/sessions', methods=['POST'])
+def log_in() -> str:
+    """ Logs in user and returns id """
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+
+    msg = {"email": email, "message": "logged in"}
+    response = jsonify(msg)
+
+    response.set_cookie("session_id", session_id)
+
+    return response
+
+
+@app.route('/sessions', methods=['DELETE'])
+def log_out() -> str:
+    """Find users with the session id.
+    if user is there destroy session d redirect the user to GET /.
+    If the user does not exist, respond with a 403 HTTP status.
+    """
+    session_id = request.cookies.get("session_id", None)
+
+    if session_id is None:
+        abort(403)
+
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user is None:
+        abort(403)
+
+    AUTH.destroy_session(user.id)
+
+    return redirect('/')
+
+
+@app.route('/profile', methods=['GET'])
+def profile() -> str:
+    """ If the user exist, then respond with 200 HTTP status and a JSON Payload
+    else 403 HTTP status.
+    """
+    session_id = request.cookies.get("session_id", None)
+
+    if session_id is None:
+        abort(403)
+
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user is None:
+        abort(403)
+
+    msg = {"email": user.email}
+
+    return jsonify(msg), 200
+
+
+@app.route('/reset_password', methods=['POST'])
+def reset_password() -> str:
+    """If mail is not registered, then 403 status code.
+    else, generate a token and respond with a
+    200 HTTP status and JSON Payload
+    """
+    try:
+        email = request.form['email']
+    except KeyError:
+        abort(403)
+
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+
+    msg = {"email": email, "reset_token": reset_token}
+
+    return jsonify(msg), 200
+
+
+@app.route('/reset_password', methods=['PUT'])
+def update_password() -> str:
+    """ PUT /reset_password
+    Updates password with the reset tokens
+    Return:
+        - 400 bad req
+        - 403 reset token
+        - 200
+    """
+    try:
+        email = request.form['email']
+        reset_token = request.form['reset_token']
+        new_password = request.form['new_password']
+    except KeyError:
+        abort(400)
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except ValueError:
+        abort(403)
+
+    msg = {"email": email, "message": "Password updated"}
+    return jsonify(msg), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
